b4174458a11c9dbfe2d7d6e761345e6b85cdc10c
KianSalehi <kian.k.salehi@gmail.com>
2020-10-09 21:49:27 -0400

    Update commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const{prefix,
 token}=require("./config.json");
 const ytdl = require("ytdl-core");
 const client = new Discord.Client();
+client.login(process.env.BOT_TOKEN);
 const queue = new Map();
 
 client.once("ready", () => {
@@ -134,4 +135,4 @@ function list(message, serverQueue){
         message.channel.send((i)+" - "+serverQueue.songs[i].title+"\n")
     return;
 }
-client.login(process.env.BOT_TOKEN);
+
