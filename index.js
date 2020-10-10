97c09e4a1ffe83cfdc7a5f2c1e7ee5733cacacf7
KianSalehi <kian.k.salehi@gmail.com>
2020-10-09 21:34:44 -0400

    Update commit

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const ytdl = require("ytdl-core");
 const client = new Discord.Client();
 const queue = new Map();
 
-client.login(token);
 client.once("ready", () => {
     console.log("Ready!");
 });
